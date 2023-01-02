9354fe9c10eb31f0978d877e90f6a7ef7fa98d44
김지호 <crsn1111@gmail.com>
2023-01-02 22:12:33 +0900

    alias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@test': path.join(__dirname, 'src/')
+      }
+    }
+  }
 })
